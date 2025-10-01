45f6b17eab49b36ae8ca6be6ecbeed4eebc2fa9a
Ashwin Pawar <er.ashwinpawar@gmail.com>
2025-10-01 16:39:11 +0530

    Update contact_book_app.py

--- Python/Practice_Projects/contact_book_app.py
+++ Python/Practice_Projects/contact_book_app.py
@@ -1,6 +1,6 @@
-contacts = {} #lsit
+contacts = {} 
 
-while True: #cant close until user dont want 
+while True: 
     print("\n CONTACT BOOK APP")
     print("1. Add Contact")
     print("2. View Contacts")
@@ -10,36 +10,34 @@ while True: #cant close until user dont want
     print("6.Count Contacts")
     print("7.Exit")   
 
-    Choice = int(input("Enter Your Choice ="))   #variable 
+    Choice = int(input("Enter Your Choice ="))   
 
     if Choice == 1:                     
         name = input("Enter Your Name =")
-        if name in contacts:   #membership operator                 #if name is i contact then print 
+        if name in contacts:   #membership operator                
             print(f"Contact Name {name} Already Exists! ")
         else:
-            age = int(input("Enter Age ="))               # if contact is not avi then ask for information 
+            age = int(input("Enter Age ="))              
             email = input("Enter Email =")
             phone_no = input("Enter Phone Number =")   
-            contacts[name] = {"age":int(age), "email":email, "phone_no":phone_no} #update it into Contact[] -empty list created at top
+            contacts[name] = {"age":int(age), "email":email, "phone_no":phone_no} 
             print(f"Contact name {name} has been updated Succesfully")
     
     elif Choice == 2:
         name = input("Enter the Contact to view =")
-        if name in contacts:  #check 1st contact is avi or not
-            contact = contacts[name]   # if contact is avilable
-            print (f"Name:{name}, Age:{age}, phone_no:{phone_no}")  #show contact
+        if name in contacts: 
+            contact = contacts[name]   
+            print (f"Name:{name}, Age:{age}, phone_no:{phone_no}") 
         else:
             print("Contact not Found!!")
 
-    elif Choice == 3 :  #update contact
+    elif Choice == 3 :  
         name = input("Enter the Contact to Update =")
         if name in contacts:
             age = (input("Update Age ="))
             email = input("Update email =")
             phone_no = (input("Update phone_no="))
             contacts[name] = {"age":int(age), "email":email, "phone_no":phone_no}
-            # contactme jo key present hai usme values koupdate kardo means age, email and phone_no is value and name is key = key-value pair
-
             print("Contact not Found")
 
     elif Choice == 4:
@@ -47,27 +45,33 @@ while True: #cant close until user dont want
         if name in contacts:
             del contacts[name]
             print(f"Contact Name {name}  has been Deleted Successfully ")
-
         else:
             print("Contact not Found")
 
     elif Choice == 5:
         search_name = input("Enter the Contact to Search =")
-        found = False #if any count is matched or not and to retirve
-        for name, contact in contacts.item(): #key value pair ka sequence milta hai tuple k andar i.e name and contact
-             # name hamara key hai aur contact value aur contacts h key value pair ki list .item se hm wo sb 
-            if search_name.lower() in name.lower(): #if this condition is true then contact enter by user is avilable   
-                #check krta hai ki jo value inter ki hia user ne lower case me uska koi hissa hamare contantcs me hai ya nhi 
+        found = False 
+        for name, contact in contacts.item():
+
+            if search_name.lower() in name.lower(): 
                 print(f" Contact is Found- Name {name}, Age:{age}, email:{email},phone_no:{phone_no}")
-                found = True # ye make sure karega ki atleast ke contact toh mila
+                found = True 
             if not found:
                 print("Contact Not Found")
     elif Choice == 6:
-        print(f"Total Contact in your Book is = {len(contacts)}")  #count ka bhi use kr skte hai len se sare contact mil jayega lets see
+        print(f"Total Contact in your Book is = {len(contacts)}")
 
 
     elif Choice == 7:
         print("Thank You 😊 Program is Closing.....")
         break 
     else:
+
         print("Invalid Choice")
+
+
+
+NOTE :- In this project i make use of membership operator (key-value pair) as well as other concepts like 
+        Loops, Dictonary, Formatted String, if-elif-else  and methods like .lower() .item() .len().
+
+        I am still leanring everyday and imporving my skiils in python. (> - <)
